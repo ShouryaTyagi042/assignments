8b727965b02590e3d660e4d71cf0f2972281a390
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-12-17 22:25:08 +0530

    01-medium-times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,14 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let startTime = new Date();
+  //   console.log(startTime);
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += i;
+  }
+  let endTime = new Date();
+  return endTime - startTime;
 }
+
+console.log(calculateTime(1000000000));
