3c62d150eb17dfdbea6d3086580008f6195f2fcc
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-12-17 13:49:48 +0530

    01-easy-anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length != str2.length) return false;
+  let map = {};
+  const res = str1.split("");
+  res.forEach((element) => {
+    element = element.toLowerCase();
+    if (map[element] == null) map[element] = 1;
+    else map[element] += 1;
+  });
+  const split2 = str2.split("");
+  let flag = true;
+  split2.forEach((elem) => {
+    elem = elem.toLowerCase();
+    if (map[elem] == null || map[elem] <= 0) {
+      flag = false;
+      return;
+    } else map[elem] -= 1;
+  });
+  return flag;
 }
 
 module.exports = isAnagram;
